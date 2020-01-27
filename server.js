ba7201f12d880cc3b1e290d595f6c5d35e9e3d9d
Jsegel <segeljosh@gmail.com>
2020-01-27 12:39:47 -0600

    start of inquirer

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
-const cTable = require("console.table");
+// const cTable = require("console.table");
 const connection = require("./config/connection");
+
+inquirer
+    .prompt([
+        {
+            type: "list",
+            name: "start",
+            message: "Welcome to the Employee Management System, what would you like to do?",
+            choices: ["Enter New","View", "Update"] 
+        }
+    ]).then(function(res){
+        console.log(res)
+    })
