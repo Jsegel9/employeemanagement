8246ea9498693fbe7dfae3e91c21faf236daeabb
Jsegel <segeljosh@gmail.com>
2020-01-27 19:03:55 -0600

    adding path

--- server.js
+++ server.js
@@ -12,5 +12,25 @@ inquirer
             choices: ["Enter New","View", "Update"] 
         }
     ]).then(function(res){
-        console.log(res)
+        console.log(res.start)
+        if(res.start === "Enter New"){
+            enterNew();
+        }
+        else if(res.start === "View"){
+            view();
+        }
+        else{
+            update();
+        }
     })
+    function enterNew(){
+        console.log("entering")
+    }
+
+    function view(){
+        console.log("viewing")
+    }
+
+    function update(){
+        console.log("updating")
+    }
