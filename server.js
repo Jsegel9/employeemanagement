5f065912694d0cea714ce1d0d283c6e4220c1750
Jsegel <segeljosh@gmail.com>
2020-01-28 19:52:21 -0600

    tweaking employee enter

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mysql = require("mysql2");
 // const cTable = require("console.table");
 const connection = require("./config/connection");
 
+
 function startApp(){
 inquirer
     .prompt([
@@ -50,15 +51,65 @@ inquirer
     }
 
 function enterRole(){
-        console.log("Entering Role")
+        
+        inquirer
+        .prompt([{
+            type: "input",
+            name: "roleTitle",
+            message: "Please enter the title of the role"
+        },{
+            type: "input",
+            name: "salary",
+            message: "Please enter the role's salary"
+        },{
+            type: "input",
+            name: "deptId",
+            message: "Please enter the department ID associated with the role"
+        }
+    ]).then(function(res){
+        console.log(res)
+    })
     }
 
 function enterDepartment(){
-    console.log("Entering Department")
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "deptName",
+            message: "Please enter the department name"
+        }
+    ]).then(function(res){
+        console.log(res)
+    })
 }
 
 function enterEmployee(){
-    console.log("Entering Employee")
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "empFirst",
+            message: "Please enter the employee's first name"
+        },
+        {
+            type: "input",
+            name: "empLast",
+            message: "Please enter the employee's last name"
+        },
+        {
+            type: "input",
+            name: "roleId",
+            message: "Please enter the Role ID # associated with this employee's role"
+        },
+        {
+            type: "input",
+            name: "empMgr",
+            message: "Please enter the ID # for this employee's manager"
+        }
+    ]).then(function(res){
+        console.log(res)
+    })
 }
 
 
